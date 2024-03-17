06293ee4ce6c6671f57691538da81aa040151a84
Andry Yahoda <andry160621@gmail.com>
2024-03-17 18:39:51 +0200

    7th commit

--- HomeWorks/HomeWork7/HomeWork7_1.py
+++ HomeWorks/HomeWork7/HomeWork7_1.py
@@ -1,21 +1,9 @@
-import my_first_module
-import math
-import random
-from my_first_module import a
-from my_first_module import b
-from my_first_module import c
-from my_first_module import get_hello_world
-from my_first_module import get_name
-from my_first_module import get_country
+player_input = input("Введіть текст який ви бажаєте: ")
+with open('player_input_1', 'w') as file:
+    file.write(player_input)
+with open('player_input_1', 'r') as file:
+    txt = file.read()
+    txt_splited = txt.split()
+    print(len(txt_splited))
 
-print(a)
-print(b)
-print(c)
-get_hello_world()
-get_name()
-get_country()
-
-d = random.randint(1, 10)
-print(d)
-print(math.pow(d, a))
 
--- HomeWorks/HomeWork8/HomeWork8_1.py
+++
@@ -1,9 +0,0 @@
-player_input = input("Введіть текст який ви бажаєте: ")
-with open('player_input_1', 'w') as file:
-    file.write(player_input)
-with open('player_input_1', 'r') as file:
-    txt = file.read()
-    txt_splited = txt.split()
-    print(len(txt_splited))
-
-
